ecfdb746d5bf4cb9fe5e57851268d2c03dc772d4
yess-kozlov <yessica.ipn@gmail.com>
2024-10-20 23:31:32 -0600

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,5 +1,31 @@
-// Mostrar un mensaje si el campo está vacío
-Toast.makeText(this, getString(R.string.error_campos_vacios), Toast.LENGTH_SHORT).show()
+package com.example.dipmob
 
-// Mostrar un mensaje de éxito cuando sea completado todo
-Toast.makeText(this, getString(R.string.formulario_exitoso), Toast.LENGTH_SHORT).show()
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.example.dipmob.fragments.DoctorSelectionFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Configura la Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Carga el primer fragment (DoctorSelectionFragment) al iniciar la app
+        if (savedInstanceState == null) {
+            replaceFragment(DoctorSelectionFragment())
+        }
+    }
+
+    // Función para reemplazar fragmentos
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view, fragment)
+            .commit()
+    }
+}
